4ee307ca1cf56dca1c31558c7cf6295dc11c3617
unknown <johnjefferson.lebasora@evsu.edu.ph>
2021-12-14 06:09:31 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './Pages/Public/log-in/log-in.component';
+import { HomeComponent } from './Pages/Public/home/home.component';
+import { RegisterComponent } from './Pages/Public/register/register.component';
+import { AuthHomeComponent } from './Pages/private/auth-home/auth-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'auth/login',component:LogInComponent},
+  {path:'auth/register',component:RegisterComponent},
+  {path:'ShareTime',component:AuthHomeComponent},
+  {path:"**",redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
+
+// #pages
+import { AuthHomeComponent } from './Pages/private/auth-home/auth-home.component';
+import { LogInComponent } from './Pages/Public/log-in/log-in.component';
+import { HomeComponent } from './Pages/Public/home/home.component';
+import { RegisterComponent } from './Pages/Public/register/register.component';
+import { FrameComponent } from './components/frame/frame.component';
+
+// #angular material 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    LogInComponent,
+    HomeComponent,
+    FrameComponent,
+    RegisterComponent,
+    AuthHomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
