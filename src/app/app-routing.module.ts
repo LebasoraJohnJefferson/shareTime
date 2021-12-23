8c1862e8fa745e917b6d5bd0056e02effd23ca1c
unknown <johnjefferson.lebasora@evsu.edu.ph>
2021-12-23 09:19:28 -0800

    redesign

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LogInComponent } from './Pages/Public/log-in/log-in.component';
-import { HomeComponent } from './Pages/Public/home/home.component';
-import { RegisterComponent } from './Pages/Public/register/register.component';
-import { AuthHomeComponent } from './Pages/private/auth-home/auth-home.component';
+import { HomeComponent } from './clients/Pages/home/home.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'auth/login',component:LogInComponent},
-  {path:'auth/register',component:RegisterComponent},
-  {path:'ShareTime',component:AuthHomeComponent},
-  {path:"**",redirectTo:''}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-
-
-// #pages
-import { AuthHomeComponent } from './Pages/private/auth-home/auth-home.component';
-import { LogInComponent } from './Pages/Public/log-in/log-in.component';
-import { HomeComponent } from './Pages/Public/home/home.component';
-import { RegisterComponent } from './Pages/Public/register/register.component';
-import { FrameComponent } from './components/frame/frame.component';
 
 // #angular material 
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import { NavComponent } from './clients/components/nav/nav.component';
+import { HomeComponent } from './clients/Pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    LogInComponent,
+    NavComponent,
     HomeComponent,
-    FrameComponent,
-    RegisterComponent,
-    AuthHomeComponent,
   ],
   imports: [
     BrowserModule,
