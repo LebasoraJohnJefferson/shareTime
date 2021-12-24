189160fda31a7f1f74d972caa5fb477ebc1e727a
unknown <johnjefferson.lebasora@evsu.edu.ph>
2021-12-23 17:57:34 -0800

    design 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './clients/Pages/home/home.component';
 import { SettingsComponent } from './clients/Pages/settings/settings.component';
+import { StoresComponent } from './clients/Pages/stores/stores.component';
+import { MessagesComponent } from './clients/Pages/messages/messages.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'settings',component:SettingsComponent}
+  {path:'settings',component:SettingsComponent},
+  {path:'stores',component:StoresComponent},
+  {path:'messages',component:MessagesComponent}
 ];
 
 @NgModule({
